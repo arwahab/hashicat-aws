5614b7bbcb7864af4c05d8efbed5c680b6ba8ff9
Abdul Wahab <arafeekala@gmail.com>
2020-02-19 19:15:12 +0000

    updatred variables files

--- variables.tf
+++ variables.tf
@@ -48,3 +48,13 @@ variable "placeholder" {
   default     = "placekitten.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
+
+variable "billable" {
+  default = true
+  description = "for billable AWS resources"
+}
+
+variable "department {
+  default = "devops"
+  description = "for billable AWS resources"
+}
