7cea178753bb5abfdda48ec9de95f52819c7c13a
Abdul Wahab <arafeekala@gmail.com>
2020-02-19 19:51:08 +0000

    one less zone

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,14 @@
+  
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "app.terraform.io/dchou-bdc/vpc/aws"
+  version = "2.21.0"
 
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-1", "us-west-2", "us-east-1"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["us-east-1d", "us-east-1f"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
